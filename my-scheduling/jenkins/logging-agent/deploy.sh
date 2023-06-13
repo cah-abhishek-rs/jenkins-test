9a18253ef5a4ea6958e0f7b3e6f8c3f0fdea8fb8
cah-keertan-kini <81167406+cah-keertan-kini@users.noreply.github.com>
2023-06-13 10:51:15 +0530

    removed autoapprove again

--- my-scheduling/jenkins/logging-agent/deploy.sh
+++ my-scheduling/jenkins/logging-agent/deploy.sh
@@ -20,7 +20,7 @@ cat backends/dev.hcl
 
 echo "Initializing cloudwatch policy for the cluster"
 
-terraform init -migrate-state --backend-config=backends/dev.hcl -auto-approve=false
+terraform init -migrate-state --backend-config=backends/dev.hcl
 
 echo "Plan cloudwatch policy for the cluster"
 

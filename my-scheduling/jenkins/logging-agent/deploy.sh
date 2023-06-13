f7adb05a58aaa9a870e453371e8166a2c3ace3d6
cah-keertan-kini <81167406+cah-keertan-kini@users.noreply.github.com>
2023-06-13 10:47:18 +0530

    adding auto approve

--- my-scheduling/jenkins/logging-agent/deploy.sh
+++ my-scheduling/jenkins/logging-agent/deploy.sh
@@ -20,7 +20,7 @@ cat backends/dev.hcl
 
 echo "Initializing cloudwatch policy for the cluster"
 
-terraform init --backend-config=backends/dev.hcl
+terraform init --backend-config=backends/dev.hcl -auto-approve=false
 
 echo "Plan cloudwatch policy for the cluster"
 

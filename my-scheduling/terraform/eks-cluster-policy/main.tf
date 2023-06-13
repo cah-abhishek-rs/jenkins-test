818cb36b0b7e431d3869eb1feea50cf6713251e2
cah-keertan-kini <81167406+cah-keertan-kini@users.noreply.github.com>
2023-06-13 12:46:58 +0530

    added missing variables in eks cluster policy

--- my-scheduling/terraform/eks-cluster-policy/main.tf
+++ my-scheduling/terraform/eks-cluster-policy/main.tf
@@ -11,4 +11,6 @@ module "dpp-myscheduling-eks-cluster-policy" {
   source                  = "git::https://github.com/cahcommercial/dpp-infrastructure-as-code.git//terraform/eks-cluster-policy?ref=main"
   eks_cluster_policy_name = var.eks_cluster_policy_name
   user_name               = var.user_name
+  aws_account_id          = var.aws_account_id
+  eks_cluster_name        = var.eks_cluster_name
 }

dc88cecda8f77c283a9805c0d3ef2e6ce97b6166
cah-abhishek-rs <122511461+cah-abhishek-rs@users.noreply.github.com>
2023-06-21 23:49:46 +0530

    gateway changes

---
+++ jenkins/tasks/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "${SERVICE_IMAGE} --service image"
+
+echo "Cloning dpp-mx-shared-infrastructure-as-code repository" 
+git clone https://github.com/cahcommercial/dpp-mx-shared-infrastructure-as-code.git pipeline-tools-jenkins
+
+cd pipeline-tools-jenkins
+git checkout ORN-182/gateway-pipline
+
+echo "Cloning dpp-infrastructure-as-code" 
+git clone https://github.com/cahcommercial/dpp-infrastructure-as-code.git pipeline-tools-chart
+
+cd pipeline-tools-chart
+git checkout main
